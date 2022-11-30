545be37f672e17c93f35ed1ab87e82f5273e82d0
cypirando <fabiocypriano@gmail.com>
2022-11-30 09:47:18 -0300

    atualização 3

--- js/main.js
+++ js/main.js
@@ -14,38 +14,45 @@ function exibeprojetos(listaProjetos) {
     listaProjetos.forEach(element => {
         inserirProjetos.innerHTML += `
         <div class="all-projects">
-            <div class="row">
-                <div class="name-project">
-                    <i class="fal fa-door-open"
-                        style="padding: 14px 8px 0 0; font-size: 18px;opacity: 0.6; color:#fff;"></i>
-                    <div class="bg-tree card-word initial-words-one">
-                    ${element.projectName.split('')[0]}
+                <div class="row">
+                    <div class="name-project">
+                        <div class="card-project">
+                            <i class="fal fa-door-open"
+                                style="padding: 2px 8px 0 0; font-size: 18px;opacity: 0.6; color:#fff;"></i>
+                            <div class="bg-tree card-word initial-words-one">
+                            ${element.projectName.split('')[0]}
+                            </div>
+                        </div>
+                        <div class="line-description">
+                            <div class="line-name">
+                                <h5 class="h5-name">
+                                    ${element.projectName}
+                                    <i class="fal fa-lock-alt" style="padding: 0 5px; font-size:12px;opacity: 0.6;"></i>
+                                </h5>
+                                <div class="role-project">
+                                    <span>
+                                        ${element.role}
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="description-project">
+                                <sub>${element.description}</sub>
+                            </div>
+                        </div>
                     </div>
-                    <h5 class="h5-name">
-                        ${element.projectName}
-                        <i class="fal fa-lock-alt" style="padding: 0 5px; font-size:12px;opacity: 0.6;"></i>
-                    </h5>
-                    <div class="role-project">
-                        <span>
-                            ${element.role}
-                        </span>
+                    <div class="star-project">
+                        <input type="checkbox" class="star" onclick="salvar()" name="curtir" id=""
+                            value="checkedValue">
+                        <label for="checkedValue" class="star-label">1</label>
+                    </div>
+                    <div class="time-project">
+                        <samp>${element.lastUpdate.substr(11, 5).split('-').reverse().join(':')}</samp>
                     </div>
                 </div>
-                <div class="star-project">
-                    <input type="checkbox" class="star" name="curtir" id=""
-                        value="checkedValue">
-                    <label for="checkedValue" class="star-label">1</label>
 
-                </div>
-                <div class="time-project">
-                    <samp>${element.lastUpdate.substr(11, 5).split('-').reverse().join(':')}</samp>
+                <span class="line-block"></span>
 
-                </div>
             </div>
-        </div>
-
-        <!-- linha  -->
-        <span class="line-block"></span>
         `
     });
 }
